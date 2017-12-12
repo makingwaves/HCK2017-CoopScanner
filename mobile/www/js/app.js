b6b9174399f81112495b9ed049bd04f2345f462a
Rafał Malin <rafal.malin@makingwaves.com>
2017-12-12 22:19:58 +0100

    add fallback for non existing products

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -1,31 +1,51 @@
+
+
 var app = {
-
-    resultDiv: null,
-
     initialize: function() {
-        document.addEventListener('deviceready', this.init.bind(this), false);
+        app.loadTemplate('start_scan.mst', null, function() {
+            document.querySelector("#startScan").addEventListener("touchend", app.startScan, false);            
+        });                
     },
     
-    init : function() {
-        document.querySelector("#startScan").addEventListener("touchend", this.startScan, false);
-        resultDiv = document.querySelector("#results");
+    devReady : function() {       
     },
     
     startScan: function() {
-    
-        cordova.plugins.barcodeScanner.scan(
-            function (result) {
-                var s = "Result: " + result.text + "<br/>" +
-                "Format: " + result.format + "<br/>" +
-                "Cancelled: " + result.cancelled;
-                resultDiv.innerHTML = s;
-            }, 
-            function (error) {
-                alert("Scanning failed: " + error);
+        $('#main-container').html('<div class="loader"></div>');  
+        cordova.plugins.barcodeScanner.scan(     
+            function(result) {
+                app.getProductDetails(result);
+            },
+            function(error) {
+                console.log("Scanning failed: " + error); 
             }
-        );
-    
+        );       
+    },
+
+    getProductDetails: function(data) {
+        var url = "https://coopscanner.azurewebsites.net/coop/product/" + data.text;
+        
+        $.get(url, function(data, status){
+            viewModel = {
+                name: data.name,
+                image_url: data.imageUrl
+            } 
+        }).done(function() {
+            $('.footer-content').html('');        
+            app.loadTemplate('product_details.mst', viewModel);
+        }).fail(function() {
+            $('.footer-content').html('No product found!');
+            app.initialize();
+        });
+    },
+
+    loadTemplate: function(templateName, viewModel, callback) {
+        $.get('templates/' + templateName, function(template) {
+            var rendered = Mustache.render(template, viewModel);
+            $('#main-container').html(rendered);
+            callback();
+        });
     }
-};
+}
 
 app.initialize();
